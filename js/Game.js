4429422378f707673452039410cf5cd5f974ffce
KevinCurtisDev <kpcurtis@gmail.com>
2019-08-07 00:25:17 +0100

    Initial set up of Game and Phrase classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,50 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor() {
+        this.missed = 0;
+        this.activePhrase = null;
+     }
+
+     startGame = () => {
+        //clear lis from ul
+        //reset lives
+        this.getRandomPhrase();
+        this.activePhrase = this.getRandomPhrase;
+        this.addPhraseToDisplay();
+     }
+
+     addPhraseToDisplay = () => {
+
+     }
+
+     gameOver() {
+        // displays screen overlay
+        //updates overlay h1 to win or lose message
+        //replaces start css class with win or lose css class
+     }
+
+     getRandomPhrase() {
+        //TODO: retrieve random phrase from phrase array
+     }
+
+     handleInteraction = () => {
+        //disable selected letter's on screen keyboard button
+        //add wrong css class to selected letter's button and call remove life method
+        // or add chosen css class to letter's keyboard button, call 
+        // showMatchedLetter method on phrase, call checkForWin method.
+        // if win call gameOver method.
+     }
+
+     removeLife() {
+        // removes life from scoreboard. Increment missed property.
+        //if 5 missed guesses call game over.
+     }
+
+     checkForWin() {
+        // checks if all letters are revealed.
+    }
+
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ //button event listeners
+ const startButton = document.querySelector('#btn__reset');
+ const keyboardButtonListener = document.querySelector('#qwerty');
+
+ const game = new Game();
+
+ startButton.addEventListener('click', () => {
+    game;
+    game.startGame();
+ });
+
+ keyboardButtonListener.addEventListener('click', (e) => {
+    if(e.target.class = 'key'){
+        game.handleInteraction();
+    }
+ });
